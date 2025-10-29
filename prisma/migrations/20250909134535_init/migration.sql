765103ad1d90515503e6b609d1d4e9745d6f9c34
Alejandro Lopez <alejeindrox@hotmail.com>
2025-10-29 13:29:14 -0400

    test: :construction: testing loki migration postgresql

--- prisma/migrations/20250909134535_init/migration.sql
+++ prisma/migrations/20250909134535_init/migration.sql
@@ -1,31 +1,44 @@
+-- CreateEnum
+CREATE TYPE "public"."Plan" AS ENUM ('basic', 'pro');
+
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "plan" TEXT NOT NULL DEFAULT 'basic',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "plan" "public"."Plan" NOT NULL DEFAULT 'basic',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Asset" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE "public"."Asset" (
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Asset_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Asset_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Favorite" (
+CREATE TABLE "public"."Favorite" (
     "userId" TEXT NOT NULL,
     "assetId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    PRIMARY KEY ("userId", "assetId"),
-    CONSTRAINT "Favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Favorite_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "Asset" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Favorite_pkey" PRIMARY KEY ("userId", "assetId")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User" ("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Asset"
+ADD CONSTRAINT "Asset_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Favorite"
+ADD CONSTRAINT "Favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Favorite"
+ADD CONSTRAINT "Favorite_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "public"."Asset" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
